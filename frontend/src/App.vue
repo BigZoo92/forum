f60616b6d9430dd362fc539c1410022071de78eb
BigZoo92 <givernaudenzo@gmail.com>
2024-10-16 16:31:28 +0200

    fix App bug

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -5,23 +5,8 @@
 </template>
 
 <script>
-import { fetchUsers } from '../services/userService';
 export default {
-  name: 'App',
-  data() {
-    return {
-      users: [],
-      error: null
-    };
-  },
-  async mounted() {
-    try {
-      this.users = await fetchUsers();
-    } catch (err) {
-      this.error = 'Erreur lors du chargement des utilisateurs';
-      console.error(err);
-    }
-  }
+  name: 'App'
 }
 </script>
 
