ab5fa5dc7bd2959ebdec193325d9f5f7fd69bceb
Alban de Braquilanges <alban.debraquilanges@canal-plus.com>
2024-10-16 15:10:26 +0200

    update

--- frontend/src/views/ChatRoom.vue
+++ frontend/src/views/ChatRoom.vue
@@ -1,13 +1,42 @@
 <template>
   <div>
     <h1>Forum n°1</h1>
+    <p>Regarde la console pour voir les événements de connexion.</p>
   </div>
 </template>
 
 <script>
+import { io } from 'socket.io-client';
+
 export default {
-  name: 'ChatRoom'
-}
+  name: 'ChatRoom',
+  data() {
+    return {
+      socket: null,
+    };
+  },
+  mounted() {
+    // Se connecter au serveur Socket.IO sur localhost:3000
+    this.socket = io('http://localhost:3000');
+
+    // Gérer les événements de connexion et déconnexion
+    this.socket.on('connect', () => {
+      console.log('Connecté au serveur Socket.IO');
+      // Émettre un événement de connexion
+      this.socket.emit('connection', { message: 'User connected' });
+    });
+
+    this.socket.on('disconnect', () => {
+      console.log('Déconnecté du serveur Socket.IO');
+    });
+  },
+  beforeUnmount() {
+    // Déconnecter proprement lorsque le composant est détruit
+    if (this.socket) {
+      this.socket.disconnect();
+    }
+  }
+};
 </script>
 
 <style scoped>
