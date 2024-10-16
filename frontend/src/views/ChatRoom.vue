c70609eff3edb10b37dee7ebd11b71be5d4cc237
BigZoo92 <givernaudenzo@gmail.com>
2024-10-16 12:01:30 +0200

    feat(forum): add room page

---
+++ frontend/src/views/ChatRoom.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>À propos</h1>
+    <p>Bienvenue sur la page À propos !</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ChatRoom'
+}
+</script>
+
+<style scoped>
+/* Styles spécifiques à la page */
+</style>
