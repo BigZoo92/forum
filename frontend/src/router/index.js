581fdf1bdb30761a08824a116a9f465c10d8b817
BigZoo92 <givernaudenzo@gmail.com>
2024-10-20 23:10:54 +0200

    fix router view v2

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,9 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import ChatRoom from '../views/ChatRoom.vue'
-import App from '../App.vue'
+import IndexForums from '../views/indexForums.vue'
 
 const routes = [
-  { path: '/', name: 'App', component: App },
+  { path: '/', name: 'App', component: IndexForums },
   { path: '/chat-room/:id', name: 'ChatRoom', component: ChatRoom }
 ]
 
